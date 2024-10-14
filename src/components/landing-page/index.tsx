cbee43853d91fc35560c411b6ea499060a443ae6
shaan-m0 <shaan@m0.ventures>
2024-10-15 00:13:00 +0530

    added arcade demo

--- src/components/landing-page/index.tsx
+++ src/components/landing-page/index.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { Player } from "video-react";
+// import { Player } from "video-react";
 import "video-react/styles/scss/video-react.scss";
 
+import { ArcadeEmbed } from "../arcade-demo";
 import FAQ from "./faq";
 import HeroSection from "./hero-section";
 import LogoPricingSection from "./pricing-section";
@@ -20,13 +21,14 @@ export default function LandingPage() {
         <h1 className="mb-12 text-center text-3xl leading-7 md:text-4xl lg:text-5xl">
           Watch the Demo
         </h1>
-        <div className="mx-auto h-auto w-auto md:w-[800px] rounded-lg border-[10px] border-default-200">
-          <Player
+        <div className="mx-auto h-auto w-auto rounded-lg border-[10px] border-default-200 ">
+          <ArcadeEmbed />
+          {/* <Player
             height={400}
             width={400}
             src="https://utfs.io/f/4HrWTRZRvKfTywDmxrluea5qw0p1nU82diECQ36vfLrKtTkW"
             poster="https://utfs.io/f/4HrWTRZRvKfTvCjFd3020XYgS7jkN8zTsJGcpnOF9KqLexaE"
-          />
+          /> */}
         </div>
       </div>
       <WallOfLogos />
